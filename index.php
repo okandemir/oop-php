473b7c89af6d03a5bb2b30147d5641369b269ad0
Okan DEMİR <bmokandemir@hotmail.com.tr>
2020-04-23 14:15:43 +0300

    deleted some comments

--- index.php
+++ index.php
@@ -3,18 +3,6 @@ require_once "load.php";
 
 use Fridge\Main\Fridge;
 use Fridge\Goods\Cola;
-/*
- *
- *
- * Tek çeşit kutu içecek (Örneğin: 33cl kutu kola) alabilen bir meşrubat dolabı dizaynı yapınız. Dolap 3 raftan oluşmakta ve her raf 20 adet kutu içecek alabilmektedir.
- *
- * Dolaptan aynı anda sadece 1 adet kutu içecek alınabilir veya koyulabilir.
- * Dolabın kapısı açık ya da kapalı olabilir.
- * Dolap tamamen boş, kısmen dolu veya tamamen dolu durumlarından birinde olabilir.
- * Dolabın boş alan kontrolünü yapabilmesi ve dolduğunda daha fazla kutu eklenmemesi gerekmektedir.
- *
- *
- * */
 
 runTest();
 
